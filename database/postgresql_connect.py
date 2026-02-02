260215d68e322d8dbcdc5fb28befaa8abedefd53
Hoang Dinh Duy <duycp102k3@gmail.com>
2026-02-02 14:49:17 +0700

    ADD: connect and create database

---
+++ database/postgresql_connect.py
@@ -0,0 +1,45 @@
+import psycopg2
+
+
+class PostgresConnect:
+    def __init__(self, host, port, user, password, database="postgres"):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+
+        # Cấu hình cho psycopg2
+        self.config = {
+            'host': host,
+            'port': port,
+            'user': user,
+            'password': password,
+            'database': database
+        }
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            # Sử dụng psycopg2 để kết nối
+            self.connection = psycopg2.connect(**self.config)
+            self.cursor = self.connection.cursor()
+            print(f'--------------Connected to POSTGRES (DB: {self.database})---------------')
+            return self.connection, self.cursor
+        except psycopg2.Error as e:
+            raise Exception(f'------------Fail to connect Postgres: {e}--------------') from e
+
+    def close(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection and self.connection.closed == 0:
+            self.connection.close()
+            print("-----------POSTGRES Close Connection----------")
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
---
+++ database/schema_manager.py
@@ -0,0 +1,53 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+def create_postgres_schema(connection, cursor, db_password):
+    database = "vn_stock_data"
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+    try:
+        cursor.execute(f"DROP DATABASE IF EXISTS {database}")
+        print(f"--------DROP database: {database} in POSTGRES---------")
+
+        cursor.execute(f"CREATE DATABASE {database}")
+        print(f"--------CREATE database: {database} in POSTGRES--------")
+
+    except psycopg2.Error as e:
+        raise Exception(f"-------Failed to CREATE DB: ERROR : {e}--------") from e
+
+    dsn_params = connection.get_dsn_parameters()
+    db_user = dsn_params.get('user')
+    db_host = dsn_params.get('host')
+    db_port = dsn_params.get('port')
+
+    cursor.close()
+    connection.close()
+
+    print(f"--------Reconnecting to new database: {database}--------")
+
+    try:
+        # Tạo kết nối mới tới database
+        new_connection = psycopg2.connect(
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+            database=database
+        )
+        new_cursor = new_connection.cursor()
+
+        with open("/home/hoangduy/PycharmProjects/stock-etl-dwh/src/sql/schema.sql", 'r') as f:
+            sql_script = f.read()
+            new_cursor.execute(sql_script)
+            print("-----Executed Schema Script------")
+
+        new_connection.commit()
+        print("-----CREATED POSTGRES SCHEMA------")
+
+        return new_connection, new_cursor
+
+    except psycopg2.Error as e:
+        if 'new_connection' in locals() and new_connection:
+            new_connection.rollback()
+        raise Exception(f"-------Failed to CREATE POSTGRES SCHEMA: ERROR : {e}--------") from e
